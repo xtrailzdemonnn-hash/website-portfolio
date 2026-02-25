ae3c53c12ad587d6ca30fe6ea9c5f92181cec260
xtrailzdemonnn-hash <xtrailzdemonnn@gmail.com>
2026-02-25 21:53:14 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // (Ini contoh jika pakai React, biarkan sesuai bawaan Anda)
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/website-portfolio/', // <--- Tambahkan baris ini. Pastikan nama repo sama persis.
-  plugins: [react()],
-})
